2d295f8815fa1dd6c4513a97e6124bfd775989b0
Allan Wang <me@allanwang.ca>
2020-09-12 20:04:51 -0700

    Start adding time group api

--- time/src/main/kotlin/ca/allanwang/discord/bot/time/TimeApi.kt
+++ time/src/main/kotlin/ca/allanwang/discord/bot/time/TimeApi.kt
@@ -2,12 +2,14 @@ package ca.allanwang.discord.bot.time
 
 import ca.allanwang.discord.bot.firebase.setValue
 import ca.allanwang.discord.bot.firebase.single
+import ca.allanwang.discord.bot.firebase.singleSnapshot
 import com.gitlab.kordlib.common.entity.Snowflake
 import com.google.common.flogger.FluentLogger
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import java.awt.Color
 import java.time.format.DateTimeFormatter
+import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -27,9 +29,18 @@ class TimeApi @Inject constructor(
     private val timeRef: DatabaseReference
         get() = firebaseDatabase.reference.child("time")
 
-    suspend fun getTime(id: Snowflake): String? =
-        timeRef.child(id.value).single<String>()
+    suspend fun getTime(id: Snowflake): TimeZone? =
+        timeRef.child(id.value).single<String>()?.let { TimeZone.getTimeZone(it) }
 
-    suspend fun saveTime(id: Snowflake, value: String): Boolean =
-        timeRef.child(id.value).setValue(value)
+    suspend fun saveTime(id: Snowflake, value: TimeZone): Boolean =
+        timeRef.child(id.value).setValue(value.id)
+
+    suspend fun groupTimes(group: Snowflake): List<TimeZone> {
+        return timeRef.child(group.value)
+            .singleSnapshot().children
+            .map { it.getValue(String::class.java) }
+            .toSet()
+            .mapNotNull { TimeZone.getTimeZone(it) }
+            .sortedBy { it.rawOffset }
+    }
 }
--- time/src/main/kotlin/ca/allanwang/discord/bot/time/TimeBot.kt
+++ time/src/main/kotlin/ca/allanwang/discord/bot/time/TimeBot.kt
@@ -5,6 +5,7 @@ import com.gitlab.kordlib.core.Kord
 import com.gitlab.kordlib.core.event.message.MessageCreateEvent
 import com.gitlab.kordlib.core.on
 import com.google.common.flogger.FluentLogger
+import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -19,10 +20,37 @@ class TimeBot @Inject constructor(
         val timeRegex = Regex("(1[0-2]|0?[1-9])(?::([0-5][0-9]))?\\s*([AaPp][Mm])?")
     }
 
+    private data class TimeEntry(val hour: Int, val minute: Int, val pm: Boolean?) {
+        override fun toString(): String = buildString {
+            append(hour)
+            append(':')
+            append(minute.toString().padStart(2, '0'))
+            if (pm != null) {
+                append(' ')
+                append(if (pm) "pm" else "am")
+            }
+        }
+    }
+
+    private fun MatchResult.toTimeEntry(): TimeEntry? {
+        // Disallow general numbers as timestamps
+        if (groupValues[2].isEmpty() && groupValues[3].isEmpty()) return null
+        val hour = groupValues[1].toInt()
+        val minute = groupValues[2].toIntOrNull() ?: 0
+        val pm = when (groupValues[3].toLowerCase(Locale.US)) {
+            "am" -> false
+            "pm" -> true
+            else -> null
+        }
+        return TimeEntry(hour, minute, pm)
+    }
+
     override suspend fun Kord.attach() {
         on<MessageCreateEvent> {
             if (message.author?.isBot == true) return@on
-
+            val times = timeRegex.findAll(message.content).mapNotNull { it.toTimeEntry() }.toList()
+            if (times.isEmpty()) return@on
+            logger.atInfo().log("Times matched %s", times)
         }
     }
 }
