4c97898b08d5b0c2dd78026d811f5b851fee077d
Allan Wang <me@allanwang.ca>
2020-09-27 03:20:05 -0700

    Add credits for word bank

--- cinco/src/main/kotlin/ca/allanwang/discord/bot/cinco/game/WordBank.kt
+++ cinco/src/main/kotlin/ca/allanwang/discord/bot/cinco/game/WordBank.kt
@@ -17,7 +17,7 @@ class WordBank @Inject constructor() {
     private val words: Set<String> =
         this::class.java.classLoader.getResourceAsStream(WORD_FILE)?.bufferedReader()?.use { reader ->
             reader.lineSequence().filter { !it.startsWith('#') }.onEach {
-                if (it.length != 5) logger.atWarning().log("")
+                if (it.length != 5) logger.atWarning().log("Non 5 letter word '%s'", it)
             }.toSet()
         } ?: run {
             logger.atWarning().log("Could not load %s", WORD_FILE)
